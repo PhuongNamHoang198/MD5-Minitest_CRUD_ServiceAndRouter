6fd984a08cc556f264176e60b4af4fd30e9ab2c0
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-10-24 09:02:51 +0700

    init

---
+++ src/app/product/product-edit/product-edit.component.ts
@@ -0,0 +1,41 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ProductService} from "../../service/product.service";
+import {ProductListComponent} from "../product-list/product-list.component";
+import {Product} from "../product";
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit {
+  @Input("id-edit-product") id : number = 0;
+
+  productForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+    price: new FormControl(),
+    description: new FormControl(),
+  });
+
+  product : Product = {
+    id: 1,
+    name: '',
+    price: 1,
+    description: ''
+  }
+
+  constructor(private productListComponent : ProductListComponent,private productService : ProductService) { }
+
+  ngOnInit(): void {
+    this.product = this.productListComponent.productEdit;
+  }
+
+  submit() {
+    const productEdit = this.productForm.value;
+    // @ts-ignore
+    this.productService.editProduct(this.product.id,productEdit);
+    this.productForm.reset();
+  }
+}
---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../service/product.service";
+import {Product} from "../product";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  productEdit: Product = {
+    id: 1,
+    name: '',
+    price: 1,
+    description: ''
+  };
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  findProductById(id: number) {
+    // console.log(this.productService.findProductById(id));
+    this.productEdit = this.productService.findProductById(id);
+    console.log(this.productEdit);
+  }
+
+  getAll() {
+    this.products = this.productService.getAll();
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id);
+    this.products = this.productService.getAll();
+  }
+}
